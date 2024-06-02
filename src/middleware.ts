64dea67f8d8fe1e34313e872ab2a684609a625be
CTHua <illiew2470@gmail.com>
2024-06-02 21:51:13 +0800

    style: prettier

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,35 @@
-import { RedirectStatusCode } from 'next/dist/client/components/redirect-status-code'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { RedirectStatusCode } from "next/dist/client/components/redirect-status-code";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 type PrivilegeDTO = {
-    "privilege": string
-}
+  privilege: string;
+};
 
 const getPrivilege = async (request: NextRequest): Promise<PrivilegeDTO> => {
-    const cookie = request.headers.get('cookie')
-    const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/users/privilege`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'cookie': cookie || '',
-        },
-    })
-    
-    return await response.json()
-}
+  const cookie = request.headers.get("cookie");
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/users/privilege`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        cookie: cookie || "",
+      },
+    }
+  );
+
+  return await response.json();
+};
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/superuser')) {
-    const privilege = await getPrivilege(request)
-    if (privilege.privilege !== 'superuser') {
-        console.log(privilege)
-      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/`, { status: RedirectStatusCode.TemporaryRedirect })
+  if (request.nextUrl.pathname.startsWith("/superuser")) {
+    const privilege = await getPrivilege(request);
+    if (privilege.privilege !== "superuser") {
+      console.log(privilege);
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/`, {
+        status: RedirectStatusCode.TemporaryRedirect,
+      });
     }
   }
 }
