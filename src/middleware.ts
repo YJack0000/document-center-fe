42deac6fe2260a2006675d1a89ff0dd1e0ffe814
CTHua <illiew2470@gmail.com>
2024-06-02 22:11:06 +0800

    feat: update middleware to check if user login

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,11 +23,32 @@ const getPrivilege = async (request: NextRequest): Promise<PrivilegeDTO> => {
 };
 
 export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  if (
+    pathname.startsWith("/login") || // exclude login page
+    pathname.startsWith("/_next") || // exclude Next.js internals
+    pathname.startsWith("/api") || //  exclude all API routes
+    pathname.startsWith("/static") || // exclude static files
+    pathname.startsWith("/favicon.ico") // exclude favicon
+  ) {
+    return NextResponse.next();
+  }
+
   if (request.nextUrl.pathname.startsWith("/superuser")) {
     const privilege = await getPrivilege(request);
     if (privilege.privilege !== "superuser") {
       console.log(privilege);
       return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/`, {
+        status: RedirectStatusCode.TemporaryRedirect,
+      });
+    }
+  }
+
+  // check if the user is logged in
+  if (pathname.startsWith("/")) {
+    const cookie = request.headers.get("cookie");
+    if (!cookie) {
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/login`, {
         status: RedirectStatusCode.TemporaryRedirect,
       });
     }
