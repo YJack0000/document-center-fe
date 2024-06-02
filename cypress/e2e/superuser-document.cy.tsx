492ebb93ce86da5df2977ebb3e374a78974d11e2
yui0303 <iampeter62@gmail.com>
2024-06-03 01:32:32 +0800

    fix: change to visit `http://localhost:3000/login`

--- cypress/e2e/superuser-document.cy.tsx
+++ cypress/e2e/superuser-document.cy.tsx
@@ -1,5 +1,5 @@
 describe("Superuser AllDocument Table", ()=> {
     it("successfully loads", ()=> {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000/login')
     })
 }) 
