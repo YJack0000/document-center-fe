3809d5efbe086fd15a142ffe069237a3b2bc6865
CTHua <illiew2470@gmail.com>
2024-06-03 12:45:30 +0800

    feat: update public document page

--- src/app/public/documents/page.tsx
+++ src/app/public/documents/page.tsx
@@ -2,18 +2,19 @@
 
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import DocumentTableWithPagination from "@/components/documents/documentTableWithPagination";
 
 const documentId = "f44acfb7-2062-4344-a277-467b541a33e9";
 
 export default function PublicPage() {
   const router = useRouter();
-
   return (
-    <div>
-        <h1 className="text-4xl font-bold my-4 w-4/5">Document Title</h1>
-        <Button onClick={() => {
-            router.push(`/public/documents/id/${documentId}`);
-        }}> {documentId} </Button>
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <div className="flex flex-col h-screen p-10 w-full">
+        <h1 className="text-4xl font-bold">公開文件</h1>
+        <hr className="my-6 w-full" />
+        <DocumentTableWithPagination type="public_all" />
+      </div>
     </div>
   );  
 }
