4aa48586430a43f4f1cb014e17cab8d960aa2d08
CTHua <illiew2470@gmail.com>
2024-06-03 12:43:05 +0800

    feat: integrate all document table

---
+++ src/components/documents/documentTableWithPagination.tsx
@@ -0,0 +1,119 @@
+import useSWR from "swr";
+import { DataTable } from "./documentTable";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+
+const DocumentTableWithPagination = ({
+  type,
+}: {
+  type: "me" | "public_all";
+}) => {
+  const [pageIndex, setPageIndex] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const route =
+    type === "me" ? "/api/documents/me" : "/api/documents/public/all";
+
+  const { data, error, isLoading } = useSWR(
+    `${route}?page=${pageIndex}&limit=${pageSize}`,
+    async (url: string) => {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      return response.json();
+    }
+  );
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="flex items-center justify-between flex-col gap-2">
+      <DataTable data={data.data} />
+      <div className="flex items-center justify-between px-2">
+        <div className="flex items-center space-x-6 lg:space-x-8">
+          <div className="flex items-center space-x-2">
+            <p className="text-sm font-medium">Rows per page</p>
+            <Select
+              value={`${pageSize}`}
+              onValueChange={(value) => {
+                setPageSize(Number(value));
+              }}
+            >
+              <SelectTrigger className="h-8 w-[70px]">
+                <SelectValue placeholder={pageSize} />
+              </SelectTrigger>
+              <SelectContent side="top">
+                {[10, 20, 30, 40, 50].map((pageSize) => (
+                  <SelectItem key={pageSize} value={`${pageSize}`}>
+                    {pageSize}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+            Page {pageIndex} of {data.totalPage}
+          </div>
+          <div className="flex items-center space-x-2">
+            <Button
+              variant="outline"
+              className="hidden h-8 w-8 p-0 lg:flex"
+              onClick={() => setPageIndex(1)}
+              disabled={pageIndex === 1}
+            >
+              <span className="sr-only">Go to first page</span>
+              <DoubleArrowLeftIcon className="h-4 w-4" />
+            </Button>
+            <Button
+              variant="outline"
+              className="h-8 w-8 p-0"
+              onClick={() => setPageIndex(pageIndex > 1 ? 1 : pageIndex - 1)}
+              disabled={pageIndex === 1}
+            >
+              <span className="sr-only">Go to previous page</span>
+              <ChevronLeftIcon className="h-4 w-4" />
+            </Button>
+            <Button
+              variant="outline"
+              className="h-8 w-8 p-0"
+              onClick={() => {
+                setPageIndex(
+                  pageIndex + 1 > data.totalPage
+                    ? data.totalPage
+                    : pageIndex + 1
+                );
+              }}
+              disabled={pageIndex === data.totalPage}
+            >
+              <span className="sr-only">Go to next page</span>
+              <ChevronRightIcon className="h-4 w-4" />
+            </Button>
+            <Button
+              variant="outline"
+              className="hidden h-8 w-8 p-0 lg:flex"
+              onClick={() => setPageIndex(data.totalPage)}
+              disabled={pageIndex === data.totalPage}
+            >
+              <span className="sr-only">Go to last page</span>
+              <DoubleArrowRightIcon className="h-4 w-4" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DocumentTableWithPagination;
