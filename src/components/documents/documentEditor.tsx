e37dbde3d7c181ca0f19ee1d4ea445bd54cb5434
CTHua <illiew2470@gmail.com>
2024-06-03 12:14:30 +0800

    feat: document editor component

---
+++ src/components/documents/documentEditor.tsx
@@ -0,0 +1,58 @@
+"use client";
+import useDocument from "@/hooks/useDocument";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+
+const DocumentEditor = ({ documentId }: { documentId: string }) => {
+  const router = useRouter();
+  const { document, error, isLoading } = useDocument(documentId);
+  const [title, setTitle] = useState(document.title);
+  const [content, setContent] = useState(document.content);
+
+  const handleSave = async () => {
+    await fetch(`/api/documents/${documentId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, content }),
+    });
+    router.push("/documents/me");
+  };
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <div className="flex flex-col w-full gap-5">
+      <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="id">文件ID</Label>
+        <Input id="id" type="text" value={document.id} disabled />
+      </div>
+
+      <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="title">標題</Label>
+        <Input
+          id="title"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="id">內文</Label>
+        <MDEditor
+          value={content}
+          onChange={(value) => setContent(value as string)}
+        />
+      </div>
+      <p>{document.status}</p>
+      <Button onClick={handleSave}>儲存</Button>
+    </div>
+  );
+};
+
+export default DocumentEditor;
