4edf116812c453268c4ae8b38dada6e80630d68d
CTHua <illiew2470@gmail.com>
2024-06-03 12:45:30 +0800

    feat: update public document page

--- src/app/public/documents/page.tsx
+++ src/app/public/documents/page.tsx
@@ -1,10 +1,14 @@
-import { DataTablePagination } from "@/components/documents/dataTablePagination";
-import { DataTableDemo } from "@/components/documents/documentTable";
+"use client";
+import DocumentTableWithPagination from "@/components/documents/documentTableWithPagination";
 
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-4">
-      <DataTableDemo />
+      <div className="flex flex-col h-screen p-10 w-full">
+        <h1 className="text-4xl font-bold">公開文件</h1>
+        <hr className="my-6 w-full" />
+        <DocumentTableWithPagination type="public_all" />
+      </div>
     </div>
   );
 }
