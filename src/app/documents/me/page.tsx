e84700d0ac5a539523a6de4deb95674ad1f387b6
CTHua <illiew2470@gmail.com>
2024-06-04 16:00:15 +0800

    feat: add add_document in my document page

--- src/app/documents/me/page.tsx
+++ src/app/documents/me/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import AddDocumentBtn from "@/components/documents/addDocumentBtn";
 import DocumentTableWithPagination from "@/components/documents/documentTableWithPagination";
 
 export default function Page() {
@@ -6,6 +7,9 @@ export default function Page() {
     <div className="flex flex-col h-screen p-10 w-full">
       <h1 className="text-4xl font-bold">我的文件</h1>
       <hr className="my-6 w-full" />
+      <div className="w-full flex justify-end mb-2">
+        <AddDocumentBtn />
+      </div>
       <DocumentTableWithPagination type="me" />
     </div>
   );
