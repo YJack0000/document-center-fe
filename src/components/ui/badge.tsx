5af9400a525fa10c23b6e35d05882899587098ac
CTHua <illiew2470@gmail.com>
2024-06-05 00:43:26 +0800

    feat: update default ui component

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,6 +15,8 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        success: "text-green-600 bg-green-200",
+        warning: "text-yellow-600 bg-yellow-200",
       },
     },
     defaultVariants: {
