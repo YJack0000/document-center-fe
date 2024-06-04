6c97eb8552d3d4b4464d741aebb57b547aca7baa
CTHua <illiew2470@gmail.com>
2024-06-04 16:33:13 +0800

    style: formatter

--- src/components/documents/documentEditor.tsx
+++ src/components/documents/documentEditor.tsx
@@ -55,68 +55,67 @@ const subChild = {
   buttonProps: { "aria-label": "Insert title" },
 };
 
-
 const DocumentEditor = ({ documentId }: { documentId: string }) => {
-    const router = useRouter();
-    const { document, error, isLoading } = useDocument(documentId);
-    const [title, setTitle] = useState(document?.title);
-    const [content, setContent] = useState(document?.content);
+  const router = useRouter();
+  const { document, error, isLoading } = useDocument(documentId);
+  const [title, setTitle] = useState(document?.title);
+  const [content, setContent] = useState(document?.content);
 
-    useEffect(() => {
-        setTitle(document?.title);
-        setContent(document?.content);
-    }, [document]);
+  useEffect(() => {
+    setTitle(document?.title);
+    setContent(document?.content);
+  }, [document]);
 
-    const handleSave = async () => {
-        await fetch(`/api/documents/${documentId}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ title, content }),
-        });
-        router.push("/documents/me");
-    };
+  const handleSave = async () => {
+    await fetch(`/api/documents/${documentId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, content }),
+    });
+    router.push("/documents/me");
+  };
 
-    if (isLoading) return <p>載入文件中...</p>;
-    if (error) {
-        setTimeout(() => {
-            router.push("/documents/me");
-        }, 3000);
+  if (isLoading) return <p>載入文件中...</p>;
+  if (error) {
+    setTimeout(() => {
+      router.push("/documents/me");
+    }, 3000);
 
-        return <p>文件載入錯誤，三秒後返回...</p>;
-    }
+    return <p>文件載入錯誤，三秒後返回...</p>;
+  }
 
-    return (
-        <div className="flex flex-col w-full gap-5">
-            <div className="w-full flex gap-2 justify-end">
-                <Button onClick={handleSave}>儲存</Button>
-                <SendReviewDialog documentId={document.id} />
-            </div>
-            <div className="grid w-full items-center gap-1.5">
-                <Label htmlFor="id">文件ID</Label>
-                <Input id="id" type="text" value={document.id} disabled />
-            </div>
+  return (
+    <div className="flex flex-col w-full gap-5">
+      <div className="w-full flex gap-2 justify-end">
+        <Button onClick={handleSave}>儲存</Button>
+        <SendReviewDialog documentId={document.id} />
+      </div>
+      <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="id">文件ID</Label>
+        <Input id="id" type="text" value={document.id} disabled />
+      </div>
 
-            <div className="grid w-full items-center gap-1.5">
-                <Label htmlFor="title">標題</Label>
-                <Input
-                    id="title"
-                    type="text"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-            </div>
-            <div className="grid w-full items-center gap-1.5">
-                <Label htmlFor="id">內文</Label>
-                <MDEditor
-                    value={content}
-                    onChange={(value) => setContent(value as string)}
-                    commands={[...commands.getCommands(), commands.group([], subChild)]}
-                />
-            </div>
-        </div>
-    );
+      <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="title">標題</Label>
+        <Input
+          id="title"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="id">內文</Label>
+        <MDEditor
+          value={content}
+          onChange={(value) => setContent(value as string)}
+          commands={[...commands.getCommands(), commands.group([], subChild)]}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentEditor;
