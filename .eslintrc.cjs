43af1cd265a4d5c14908ad2511b411e2cd2a08c7
YJack0000 <jackleagends@gmail.com>
2024-06-05 17:27:34 +0800

    feat: make prettier integrate to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,16 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['prettier', 'react-refresh'],
   rules: {
+    'prettier/prettier': ['error'],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
