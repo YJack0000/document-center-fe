eda81e7fb9f1ffbef9f5a2666055da0576a264bb
CTHua <illiew2470@gmail.com>
2024-06-05 16:06:35 +0800

    feat: global loading and layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Sidebar } from "@/components/sidebar"
 import { usePathname } from "next/navigation"
 import { Toaster } from "@/components/ui/toaster"
 import NextTopLoader from "nextjs-toploader"
+import { useState } from "react"
+import LoadingContext from "@/context/loadingContext"
+import { Loader } from "lucide-react"
 const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({
@@ -12,6 +15,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const [globalLoading, setGlobalLoading] = useState(false)
   const pathname = usePathname()
   // if path is /login, return the login layout
   if (pathname === "/login") {
@@ -27,10 +31,19 @@ export default function RootLayout({
     <html lang="en" data-color-mode="light">
       <body className={inter.className}>
         <NextTopLoader zIndex={1600} />
-          <div className="flex h-[100vh]">
-            <Sidebar className="max-w-60 h-screen border" />
-                        {children}
-          </div>
+        <div className="flex h-[100vh]">
+          <Sidebar className="max-w-60 h-screen border" />
+          <LoadingContext.Provider value={[globalLoading, setGlobalLoading]}>
+            {globalLoading && (
+              <div className="h-[100vh] flex items-center justify-center w-full">
+                <Loader className="m-auto animate-spin" size={180} />
+              </div>
+            )}
+            <div className={`w-full flex ${globalLoading ? "hidden" : ""}`}>
+              {children}
+            </div>
+          </LoadingContext.Provider>
+        </div>
         <Toaster />
       </body>
     </html>
