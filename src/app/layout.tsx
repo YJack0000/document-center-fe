251d267cad880f8f3564e228a1b6b62629df9af4
YJack0000 <jackleagends@gmail.com>
2024-06-05 17:26:46 +0800

    feat: make title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,45 +7,53 @@ import { Toaster } from "@/components/ui/toaster"
 import NextTopLoader from "nextjs-toploader"
 import { useState } from "react"
 import LoadingContext from "@/context/loadingContext"
+import Head from "next/head";
 import { Loader } from "lucide-react"
 const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  const [globalLoading, setGlobalLoading] = useState(false)
-  const pathname = usePathname()
-  // if path is /login, return the login layout
-  if (pathname === "/login") {
+    const [globalLoading, setGlobalLoading] = useState(false)
+    const pathname = usePathname()
+    // if path is /login, return the login layout
+    if (pathname === "/login") {
+        return (
+            <html lang="en" data-color-mode="light">
+                <body className={inter.className}>
+                    <div className="flex"> {children}</div>
+                </body>
+            </html>
+        )
+    }
     return (
-      <html lang="en" data-color-mode="light">
-        <body className={inter.className}>
-          <div className="flex"> {children}</div>
-        </body>
-      </html>
+        <html lang="en" data-color-mode="light">
+            <Head>
+                <title>Document Center</title>
+                <meta
+                    name="description"
+                    content="2024 雲原生與最佳實踐期末專案 - 文件中心 - 第十組"
+                />
+            </Head>
+            <body className={inter.className}>
+                <NextTopLoader zIndex={1600} />
+                <div className="flex h-[100vh]">
+                    <Sidebar className="max-w-60 h-screen border" />
+                    <LoadingContext.Provider value={[globalLoading, setGlobalLoading]}>
+                        {globalLoading && (
+                            <div className="h-[100vh] flex items-center justify-center w-full">
+                                <Loader className="m-auto animate-spin" size={64} />
+                            </div>
+                        )}
+                        <div className={`w-full flex ${globalLoading ? "hidden" : ""}`}>
+                            {children}
+                        </div>
+                    </LoadingContext.Provider>
+                </div>
+                <Toaster />
+            </body>
+        </html>
     )
-  }
-  return (
-    <html lang="en" data-color-mode="light">
-      <body className={inter.className}>
-        <NextTopLoader zIndex={1600} />
-        <div className="flex h-[100vh]">
-          <Sidebar className="max-w-60 h-screen border" />
-          <LoadingContext.Provider value={[globalLoading, setGlobalLoading]}>
-            {globalLoading && (
-              <div className="h-[100vh] flex items-center justify-center w-full">
-                <Loader className="m-auto animate-spin" size={64} />
-              </div>
-            )}
-            <div className={`w-full flex ${globalLoading ? "hidden" : ""}`}>
-              {children}
-            </div>
-          </LoadingContext.Provider>
-        </div>
-        <Toaster />
-      </body>
-    </html>
-  )
 }
